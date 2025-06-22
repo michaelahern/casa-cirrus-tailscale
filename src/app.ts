9b8e9ebb89c3397c5f3e127a125d54a293f2cbf8
Michael Ahern <michael.ahern@gmail.com>
2025-06-22 21:04:23 +0000

    Auth Key

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
+import { TailscaleAuthKeyStack } from './auth-key-stack.js';
 import { TailscaleExitNodeStack } from './exit-node-stack.js';
 import { TailscaleRouterNodeStack } from './router-node-stack.js';
 
 const app = new cdk.App();
+
+new TailscaleAuthKeyStack(app, 'TailscaleAuthKey', {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+});
 
 new TailscaleExitNodeStack(app, 'TailscaleExitNode', {
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
