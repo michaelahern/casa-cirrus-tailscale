ac53114c5e099236633e833fdb274099d2e47b08
Michael Ahern <michael.ahern@gmail.com>
2025-06-22 00:16:54 +0000

    Initial TaskDef & AuthKey

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { TailscaleExitNode } from './stack.js';
+import { TailscaleExitNodeStack } from './stack.js';
 
 const app = new cdk.App();
-new TailscaleExitNode(app, 'TailscaleExitNode', {
+new TailscaleExitNodeStack(app, 'TailscaleExitNode', {
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
