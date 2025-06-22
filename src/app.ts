ba64d0349770f54f32fed0a055f26f0298ee7cbf
Michael Ahern <michael.ahern@gmail.com>
2025-06-22 21:07:39 +0000

    Region Checks

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,17 @@ import { TailscaleRouterNodeStack } from './router-node-stack.js';
 
 const app = new cdk.App();
 
-new TailscaleAuthKeyStack(app, 'TailscaleAuthKey', {
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-});
+if (process.env.CDK_DEFAULT_REGION === 'us-east-2') {
+    new TailscaleAuthKeyStack(app, 'TailscaleAuthKey', {
+        env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+    });
 
-new TailscaleExitNodeStack(app, 'TailscaleExitNode', {
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-});
-
-new TailscaleRouterNodeStack(app, 'TailscaleRouterNode', {
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-});
+    new TailscaleRouterNodeStack(app, 'TailscaleRouterNode', {
+        env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+    });
+}
+else {
+    new TailscaleExitNodeStack(app, 'TailscaleExitNode', {
+        env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+    });
+}
